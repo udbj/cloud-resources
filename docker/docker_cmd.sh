66fc938abf918f99fb43e239d823fc4dc8e45856
Dave Jain <jain.udbhav@gmail.com>
2020-08-10 13:54:11 +1000

    Add files via upload

---
+++ docker/docker_cmd.sh
@@ -0,0 +1,23 @@
+# pull an image from docker hub
+docker pull ubuntu
+
+# run a loaded image
+docker run ubuntu
+
+# run interactive shell for an image
+docker run -it ubuntu
+
+# list all images in workspace
+docker ps --all
+
+# force stop and remove image
+docker rm --force ubuntu
+
+# log into docker account
+docker login
+
+# logout from docker account
+docker logout
+
+
+
